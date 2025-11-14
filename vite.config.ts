d05b3e2a15ed4a70a50c38756009e93c068e5674
chenm1xuexi <837698719@qq.com>
2025-11-14 21:18:57 +0800

    项目结构初始化

--- vite.config.ts
+++ vite.config.ts
@@ -27,10 +27,7 @@ export default defineConfig({
   },
   server: {
     host: true,
-    allowedHosts: [
-      "localhost",
-      "127.0.0.1",
-    ],
+    allowedHosts: true,
     fs: {
       strict: true,
       deny: ["**/.*"],
